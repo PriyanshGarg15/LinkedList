831fcde4ae340814eabd09d80c6440b4021475ed
priyanshgarg15 <priyanshgargagra123@gmail.com>
2024-10-13 00:35:55 +0530

    first commit

--- a6_lc_2095_delete_middle_of_linked_list.java
+++ a6_lc_2095_delete_middle_of_linked_list.java
@@ -1,27 +1,30 @@
-// /**
-//  * Definition for singly-linked list.
-//  * public class ListNode {
-//  *     int val;
-//  *     ListNode next;
-//  *     ListNode() {}
-//  *     ListNode(int val) { this.val = val; }
-//  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-//  * }
-//  */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 // class Solution {
 //     public ListNode deleteMiddle(ListNode head) {
-//         if(head.next==null)
+//         if(head.next==null || head==null)
 //         {
 //             return null;
 //         }
 //         ListNode slow=head;
 //         ListNode fast=head;
-//         while(fast.next.next!=null && fast.next.next.next!=null)
+//         ListNode prev=null;
+//         while(fast!=null && fast.next!=null)
 //         {
+//             prev=slow;
 //             slow=slow.next;
 //             fast=fast.next.next;
 //         }
-//         slow.next=slow.next.next;
+//         prev.next=slow.next;
+//         slow.next=null;
 //         return head;
 //     }
 // }
